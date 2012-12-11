d0cc02f565890ae4e4e8097e8d0084bd174e3bd9
Melcosoft <dan.bykov@gmail.com>
2012-12-11 17:43:18 +0400

    Add homepage

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,7 @@ class GamesController < ApplicationController
   # GET /games
   # GET /games.json
   def index
-    @games = Game.all
+    @games = Game.paginate(page: params[:page], per_page: 18)
 
     respond_to do |format|
       format.html # index.html.erb
